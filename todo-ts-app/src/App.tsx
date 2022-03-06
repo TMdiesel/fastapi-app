1d1f839d93e1d2d24eb960e9ebf18bd14ebe3719
TMdiesel <cerezzodora2262@icloud.com>
2022-03-06 03:31:32 +0000

    feat: todoをページに表示

--- todo-ts-app/src/App.tsx
+++ todo-ts-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 type Todo = {
   value: string;
+  readonly id: number;
 };
 
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
     if (!text) return;
     const newTodo: Todo = {
       value: text,
+      id: new Date().getTime(),
     };
     setTodos([newTodo, ...todos]);
     setText("");
@@ -19,6 +21,19 @@ export const App = () => {
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
+  };
+
+  const handleOnEdit = (id: number, value: string) => {
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
   };
 
   return (
@@ -32,6 +47,19 @@ export const App = () => {
         <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
         <input type="submit" value="追加" onSubmit={handleOnSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleOnEdit(todo.id, e.target.value)}
+              ></input>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
