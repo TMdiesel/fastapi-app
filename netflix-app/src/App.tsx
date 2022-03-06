8f646e3d47284dcf26d596751ecead7cf77b621e
TMdiesel <cerezzodora2262@icloud.com>
2022-03-06 13:47:24 +0000

    feat: add row

--- netflix-app/src/App.tsx
+++ netflix-app/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { requests } from "./request";
+import { Row } from "./components/Row";
 
 function App() {
-  return <div className="App">hoge </div>;
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      {/**
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
+      **/}
+    </div>
+  );
 }
 
 export default App;
