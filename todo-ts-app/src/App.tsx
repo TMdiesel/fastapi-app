cc617b96782b21b157b95629c35122d035d33966
TMdiesel <cerezzodora2262@icloud.com>
2022-03-06 03:11:30 +0000

    feat: ステータスの設定追加

--- todo-ts-app/src/App.tsx
+++ todo-ts-app/src/App.tsx
@@ -1,13 +1,36 @@
+import React, { useState } from "react";
+
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnSubmit = () => {
+    if (!text) return;
+    const newTodo: Todo = {
+      value: text,
+    };
+    setTodos([newTodo, ...todos]);
+    setText("");
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
       </form>
     </div>
   );
