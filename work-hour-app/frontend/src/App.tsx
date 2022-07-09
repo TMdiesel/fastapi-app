840c2913be8d070cf1121ca1672a44abd2ac180e
TMdiesel <cerezzodora2262@icloud.com>
2022-07-09 01:08:47 +0000

    feat: add navbar

--- work-hour-app/frontend/src/App.tsx
+++ work-hour-app/frontend/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { readWork } from "./fetcher";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import "bulma/css/bulma.css";
 
 function App() {
+  //readWork().then((responses) =>
+  //  console.log(responses.map((response) => console.log(response)))
+  //);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav className="navbar is-success is-fixed-top">
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <NavLink to="/" className="navbar-item is-size-4">
+                開始
+              </NavLink>
+              <NavLink to="/data" className="navbar-item is-size-4 ">
+                データ一覧
+              </NavLink>
+              <NavLink to="/vis" className="navbar-item is-size-4">
+                可視化
+              </NavLink>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path={`/`} element={<div>home</div>} />
+          <Route path={`/data`} element={<div>data</div>} />
+          <Route path={`/vis`} element={<div>vis</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
