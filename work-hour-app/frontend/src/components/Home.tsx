0f9269c6c9a3fb5a1bcdbda5dd917bd31b2a8d06
TMdiesel <cerezzodora2262@icloud.com>
2022-07-09 04:36:10 +0000

    feat: add current time

--- work-hour-app/frontend/src/components/Home.tsx
+++ work-hour-app/frontend/src/components/Home.tsx
@@ -1,3 +1,18 @@
 export const Home = () => {
-  return <div>home</div>;
+  const getTime = (): string => {
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    const day = now.getDay();
+    const dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"][day];
+    const hour = ("0" + now.getHours()).slice(-2);
+    const minute = ("0" + now.getMinutes()).slice(-2);
+    return `${month}/${date} (${dayOfWeek}) ${hour}:${minute}`;
+  };
+
+  return (
+    <div>
+      <div className="is-size-1 has-text-centered">{getTime()}</div>
+    </div>
+  );
 };
